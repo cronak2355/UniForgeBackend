45d5a8be457ebe562c424aa64fc7fd8a375cbddf
권효승 <danmuji4049@gmail.com>
2025-12-31 20:17:28 +0900

    fix: 에셋 URL 절대 경로로 수정

--- src/editor/EditorLayout.tsx
+++ src/editor/EditorLayout.tsx
@@ -12,11 +12,11 @@ export default function EditorLayout() {
     const [entities] = useState<EditorEntity[]>([]);
     const [selectedEntity, setSelectedEntity] = useState<EditorEntity | null>(null);
     const [assets] = useState<Asset[]>([
-        { id: 0, name: "testAsset1", tag: "Tile", url: "TestAsset.webp", idx: -1 },
-        { id: 1, name: "testAsset2", tag: "Tile", url: "TestAsset2.webp", idx: -1 },
-        { id: 2, name: "testAsset3", tag: "Tile", url: "TestAsset3.webp", idx: -1 },
-        { id: 3, name: "placeholder", tag: "Character", url: "placeholder.png", idx: -1 },
-        { id: 4, name: "dragon", tag: "Character", url: "RedDragon.webp", idx: -1 },
+        { id: 0, name: "testAsset1", tag: "Tile", url: "/TestAsset.webp", idx: -1 },
+        { id: 1, name: "testAsset2", tag: "Tile", url: "/TestAsset2.webp", idx: -1 },
+        { id: 2, name: "testAsset3", tag: "Tile", url: "/TestAsset3.webp", idx: -1 },
+        { id: 3, name: "placeholder", tag: "Character", url: "/placeholder.png", idx: -1 },
+        { id: 4, name: "dragon", tag: "Character", url: "/RedDragon.webp", idx: -1 },
     ]);
     const [draggedAsset, setDraggedAsset] = useState<Asset | null>(null);
     const [selectedAsset, setSelectedAsset] = useState<Asset | null>(null);
