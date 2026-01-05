1d5aa7a07945bca3238d95ca13947e5a3e88e718
권효승 <danmuji4049@gmail.com>
2026-01-05 17:33:04 +0900

    fix: add missing common and library packages, fix corrupted .gitignore

---
+++ backend/src/main/kotlin/com/unifor/backend/library/controller/LibraryController.kt
@@ -0,0 +1,15 @@
+﻿package com.unifor.backend.library.controller
+
+import com.unifor.backend.library.service.LibraryService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/library")
+class LibraryController(
+    private val libraryService: LibraryService
+) {
+    @GetMapping
+    fun getLibrary(@RequestParam userId: String) =
+        libraryService.getUserLibrary(userId)
+}
+
