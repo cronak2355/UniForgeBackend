08040040d383088cabc3985f61cef93a9a83c58d
권효승 <danmuji4049@gmail.com>
2026-01-05 23:17:53 +0900

    chore: trigger redeploy and include local config changes

--- UniForgeFrontend/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import "./App.css";
-import EditorLayout from "./editor/EditorLayout";
-import { AssetsEditorPage } from './AssetsEditor';
-
-export default function App() {
-  return <AssetsEditorPage />;
-}
-
-export default App;
