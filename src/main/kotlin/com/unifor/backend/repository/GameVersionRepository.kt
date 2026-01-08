bf2f76bb53fbf5210075c73059783d768cb3c456
hyeeun32 <minhyeeun0302@gmail.com>
2026-01-08 23:53:10 +0900

    Remove legacy duplicate files (Game, GameService, GameController) after merge

--- src/main/kotlin/com/unifor/backend/repository/GameVersionRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.unifor.backend.repository
-
-import com.unifor.backend.entity.GameVersion
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface GameVersionRepository : JpaRepository<GameVersion, Long> {
-    fun findByGameIdOrderByVersionNumberDesc(gameId: Long): List<GameVersion>
-}
